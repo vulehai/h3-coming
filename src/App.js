59bd57fe4edc5730e8821f3d5d774ecccdf5956c
Hai Vu <hai.vu@dou.com.vn>
2020-03-18 16:51:07 +0700

    add avata

--- src/App.js
+++ src/App.js
@@ -1,18 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import ComingSoon from 'react-coming-soon';
-
-function App() {
-  return (
-    <ComingSoon
-      image="https://react-coming-soon.maksv.me/default-image.jpeg"
-      bgColor="#fff"
-      textColor="#000"
-      illustration="planning"
-      date="05/07/2020"
-      title="H3Corp - H3Soft - We coming soon"
-    />
-  );
+import Avatar, {ConfigProvider} from 'react-avatar';
+import SkypeSource from 'react-avatar/es/sources/Skype';
+import {createAvatarComponent, TwitterSource} from 'react-avatar';
+class App extends Component {
+  componentDidMount() {
+    document.title = 'H3Soft';
+  }
+  render() {
+    return (
+      <ComingSoon
+        image="https://react-coming-soon.maksv.me/default-image.jpeg"
+        bgColor="#fff"
+        textColor="#000"
+        illustration="development"
+        date="05/07/2020"
+        title="H3Corp - H3Soft - We coming soon"
+      >
+        <div>
+          <Avatar facebookId="100003309642690" size="60" round={true} />
+          <Avatar facebookId="100003204313279" size="60" round={true} />
+          <Avatar facebookId="100044678270829" size="60" round={true} />
+        </div>
+      </ComingSoon>
+    );
+  }
 }
 
 export default App;
